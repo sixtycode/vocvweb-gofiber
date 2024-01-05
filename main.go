87b5b41787ed733e662418bbc139bda0ea1cc49e
vocversefrontend <96647754+sixtycode@users.noreply.github.com>
2024-01-05 20:15:59 +0700

    Update main.go

--- main.go
+++ main.go
@@ -4,13 +4,25 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
 	app := fiber.New()
 
+	app.Use(cors.New())
+
+	app.Use(func(c *fiber.Ctx) error {
+		if c.Is("json") {
+			return c.Next()
+		}
+		return c.SendString("Only JSON allowed!")
+	})
+
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
+		return c.JSON(fiber.Map{
+			"message": "Hello World",
+		})
 	})
 
 	log.Fatal(app.Listen(":3000"))
