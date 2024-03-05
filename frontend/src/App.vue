516b40b3d65327f667cce518d762c53b14fac8c9
vocversefrontend <96647754+sixtycode@users.noreply.github.com>
2024-03-05 23:42:28 +0700

    create vue3 component DashboardKampus

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import DashboardKampus from './components/DashboardKampus.vue'
 </script>
 
 <template>
@@ -11,7 +11,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <DashboardKampus msg="Vite + Vue" />
 </template>
 
 <style scoped>
