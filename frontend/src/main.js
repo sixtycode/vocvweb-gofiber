2108605a201ea9ac2475d4193362503fe1a9b487
vocversefrontend <96647754+sixtycode@users.noreply.github.com>
2024-03-09 01:39:30 +0700

    update template

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,7 +3,6 @@ import App from "./App.vue";
 
 // import Bulma, Font Awesome, Font OpenSans, custom Admin css
 import "./../node_modules/Bulma/CSS/bulma.css";
-// import "./assets/js/bulma.js";
 import "./assets/css/font-awesome.min.css";
 import "./assets/css/font-opensans.css";
 import "./assets/css/admin.css";
