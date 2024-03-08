ef8be0a2da0135aaf6ba59b4c9b3c92b36486937
vocversefrontend <96647754+sixtycode@users.noreply.github.com>
2024-03-09 01:09:05 +0700

    move to src assets

--- frontend/src/components/DashboardKampus.vue
+++ frontend/src/components/DashboardKampus.vue
@@ -1,29 +1,6 @@
 <script setup></script>
 
 <template>
-  <!DOCTYPE html>
-  <html>
-    <head>
-      <meta charset="utf-8" />
-      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <title>Admin - Free Bulma template</title>
-      <link
-        rel="stylesheet"
-        href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
-      />
-      <link
-        href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700"
-        rel="stylesheet"
-      />
-      <!-- Bulma Version 0.9.0-->
-      <link
-        rel="stylesheet"
-        href="https://unpkg.com/bulma@0.9.4/css/bulma.min.css"
-      />
-      <link rel="stylesheet" type="text/css" href="../css/admin.css" />
-    </head>
-
     <body>
       <!-- START NAV -->
       <nav class="navbar is-white">
@@ -310,9 +287,7 @@
           </div>
         </div>
       </div>
-      <script async type="text/javascript" src="../js/bulma.js"></script>
     </body>
-  </html>
 </template>
 
 <style></style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from "vue";
-import "./../node_modules/Bulma/CSS/bulma.css";
 import App from "./App.vue";
+import "./../node_modules/Bulma/CSS/bulma.css";
+import "./assets/css/admin.css";
+import "./assets/css/font-awesome.min.css";
+import "./assets/css/font-opensans.css";
+import "./assets/js/bulma.js";
 
 createApp(App).mount("#app");
