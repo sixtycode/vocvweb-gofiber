5ff01c9d6651a08068e6ae1cab845bb32bb5f26c
vocversefrontend <96647754+sixtycode@users.noreply.github.com>
2024-03-09 01:14:10 +0700

    Update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,11 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+
+// import Bulma, Font Awesome, Font OpenSans, custom Admin css
 import "./../node_modules/Bulma/CSS/bulma.css";
-import "./assets/css/admin.css";
+import "./assets/js/bulma.js";
 import "./assets/css/font-awesome.min.css";
 import "./assets/css/font-opensans.css";
-import "./assets/js/bulma.js";
+import "./assets/css/admin.css";
 
 createApp(App).mount("#app");
